a13936c61dc010bb1f1f066c1651afe1bd7066fd
shravya <shravya.ks0@gmail.com>
2017-07-03 23:26:01 +0530

    Added point-to-point-fattree.h with bounding box function working

---
+++ src/point-to-point-layout/model/point-to-point-fattree.cc
@@ -0,0 +1,309 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2017 NITK Surathkal
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Shravya K.S. <shravya.ks0@gmail.com>
+ *
+ */
+
+#include "ns3/point-to-point-fattree.h"
+#include "ns3/internet-stack-helper.h"
+#include "ns3/point-to-point-helper.h"
+#include "ns3/constant-position-mobility-model.h"
+#include "ns3/point-to-point-net-device.h"
+#include "ns3/string.h"
+#include "ns3/vector.h"
+#include "ns3/log.h"
+#include "ns3/ipv4-address-generator.h"
+#include "ns3/ipv6-address-generator.h"
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("PointToPointFatTreeHelper");
+
+PointToPointFatTreeHelper::PointToPointFatTreeHelper (uint32_t numPods,
+                                                      PointToPointHelper p2pHelper)
+  : m_numPods (numPods)
+{
+  // Bounds check
+  if (numPods < 0)
+    {
+      NS_FATAL_ERROR ("Need more pods for FatTree.");
+    }
+  
+  uint32_t numEdgeSwitches = numPods/2;
+  uint32_t numAggregateSwitches = numPods/2;            // number of aggregation switch in a pod
+  uint32_t numGroups = numPods/2;		        // number of group of core switches
+  uint32_t numCoreSwitches = numPods/2;		        // number of core switch in a group
+  uint32_t numServers = numPods*numPods*numPods/4;	// number of hosts in the entire network	
+  //m_levelSwitchDevices.resize ((nLevels + 1) * numLevelSwitches);
+  //m_switchInterfaces.resize ((nLevels + 1) * numLevelSwitches);
+
+  m_servers.Create (numServers);
+  m_edgeSwitches.Create (numEdgeSwitches * numPods);
+  m_aggregateSwitches.Create (numAggregateSwitches * numPods);
+  m_coreSwitches.Create(numCoreSwitches * numGroups);
+
+  InternetStackHelper stack;
+
+  //Connect servers to edge switches
+  uint32_t hostId = 0;
+  for (uint32_t i = 0; i < numPods*numPods/2; i++)
+    {
+      for (uint32_t j = 0; j < numEdgeSwitches; j++)
+        {
+          NetDeviceContainer nd = p2pHelper.Install (m_servers.Get (hostId), m_edgeSwitches.Get (i));
+          hostId += 1;
+        }
+    }
+
+  //Connect edge switches to aggregate switches
+  for (uint32_t i = 0; i < numPods; i++)
+    {
+      for (uint32_t j = 0; j < numAggregateSwitches; j++)
+        {
+	  for (uint32_t k = 0; k < numEdgeSwitches; k++)
+            {
+              NetDeviceContainer nd = p2pHelper.Install (m_edgeSwitches.Get (i * numEdgeSwitches + k), 
+                                                         m_aggregateSwitches.Get(i * numAggregateSwitches + j));
+            }
+        }
+    }
+
+  //Connect aggregate switches to core switches
+  for (uint32_t i = 0; i < numGroups; i++)
+    {		
+      for (uint32_t j = 0; j < numCoreSwitches; j++)
+         {
+	   for (uint32_t k = 0; k < numPods; k++)
+             {	
+               NetDeviceContainer nd = p2pHelper.Install (m_aggregateSwitches.Get (k * numAggregateSwitches + i), 
+                                                          m_coreSwitches.Get(i * numCoreSwitches + j));
+              }
+          }
+    }
+
+}
+
+PointToPointFatTreeHelper::~PointToPointFatTreeHelper ()
+{
+}
+
+void
+PointToPointFatTreeHelper::InstallStack (InternetStackHelper stack)
+{
+  stack.Install (m_servers);
+  stack.Install (m_edgeSwitches);
+  stack.Install (m_aggregateSwitches);
+  stack.Install (m_coreSwitches);
+}
+
+void
+PointToPointFatTreeHelper::BoundingBox (double ulx, double uly,
+                                      double lrx, double lry)
+{
+  double xDist;
+  double yDist;
+  if (lrx > ulx)
+    {
+      xDist = lrx - ulx;
+    }
+  else
+    {
+      xDist = ulx - lrx;
+    }
+  if (lry > uly)
+    {
+      yDist = lry - uly;
+    }
+  else
+    {
+      yDist = uly - lry;
+    }
+
+  uint32_t numServers = m_numPods*m_numPods*m_numPods/4;
+  uint32_t numSwitches = m_numPods*m_numPods/2;
+
+  double xServerAdder = xDist / numServers;
+  double xEdgeSwitchAdder = xDist / numSwitches;
+  double xAggregateSwitchAdder =  xDist / numSwitches;
+  double xCoreSwitchAdder = xDist / (numSwitches/2);
+  double  yAdder = yDist / 4;  // 3 layers of switches and 1 layer of servers
+
+  //Allot servers
+  double xLoc;
+  double yLoc = yDist / 2;
+  for (uint32_t i = 0; i < numServers; ++i)
+    {
+      //xLoc = xDist / 2;
+      Ptr<Node> node = m_servers.Get (i);
+      Ptr<ConstantPositionMobilityModel> loc = node->GetObject<ConstantPositionMobilityModel> ();
+      if (loc == 0)
+        {
+          loc = CreateObject<ConstantPositionMobilityModel> ();
+          node->AggregateObject (loc);
+        }
+      Vector locVec (xLoc, yLoc, 0);
+      loc->SetPosition (locVec);
+      if(i%2 == 0)
+        {
+          xLoc += 3 * xServerAdder;
+        }
+      else
+        {
+          xLoc += 1.1 * xServerAdder;
+        }
+    }
+
+  yLoc -= yAdder;
+
+  //Allot edge switches
+  xLoc = xEdgeSwitchAdder;
+  for (uint32_t i = 0; i < numSwitches; ++i)
+    {
+      Ptr<Node> node = m_edgeSwitches.Get (i);
+      Ptr<ConstantPositionMobilityModel> loc = node->GetObject<ConstantPositionMobilityModel> ();
+      if (loc == 0)
+        {
+          loc = CreateObject<ConstantPositionMobilityModel> ();
+          node->AggregateObject (loc);
+        }
+      Vector locVec (xLoc, yLoc, 0);
+      loc->SetPosition (locVec);
+      xLoc += 2*xEdgeSwitchAdder;
+    }
+  
+  yLoc -= yAdder;
+
+  //Allot aggregate switches
+  xLoc = xAggregateSwitchAdder;
+  for (uint32_t i = 0; i < numSwitches; ++i)
+    {
+      Ptr<Node> node = m_aggregateSwitches.Get (i);
+      Ptr<ConstantPositionMobilityModel> loc = node->GetObject<ConstantPositionMobilityModel> ();
+      if (loc == 0)
+        {
+          loc = CreateObject<ConstantPositionMobilityModel> ();
+          node->AggregateObject (loc);
+        }
+      Vector locVec (xLoc, yLoc, 0);
+      loc->SetPosition (locVec);
+      xLoc += 2*xAggregateSwitchAdder;
+    }
+
+  yLoc -= yAdder;
+
+  //Allot aggregate switches
+  xLoc = xCoreSwitchAdder;
+  for (uint32_t i = 0; i < numSwitches/2; ++i)
+    {
+      Ptr<Node> node = m_coreSwitches.Get (i);
+      Ptr<ConstantPositionMobilityModel> loc = node->GetObject<ConstantPositionMobilityModel> ();
+      if (loc == 0)
+        {
+          loc = CreateObject<ConstantPositionMobilityModel> ();
+          node->AggregateObject (loc);
+        }
+      Vector locVec (xLoc, yLoc, 0);
+      loc->SetPosition (locVec);
+      xLoc += 2*xCoreSwitchAdder;
+    }
+}
+
+/*
+void
+PointToPointBCubeHelper::AssignIpv4Addresses (Ipv4Address network, Ipv4Mask mask)
+{
+  Ipv4AddressGenerator::Init (network, mask);
+  Ipv4Address v4network;
+  Ipv4AddressHelper addrHelper;
+
+  for (uint32_t i = 0; i < m_levelSwitchDevices.size (); ++i)
+    {
+      v4network = Ipv4AddressGenerator::NextNetwork (mask);
+      addrHelper.SetBase (v4network, mask);
+      for (uint32_t j = 0; j < m_levelSwitchDevices[i].GetN (); j += 2)
+        {
+          Ipv4InterfaceContainer ic = addrHelper.Assign (m_levelSwitchDevices[i].Get (j));
+          m_serverInterfaces.Add (ic);
+          ic = addrHelper.Assign (m_levelSwitchDevices[i].Get (j + 1));
+          m_switchInterfaces[i].Add (ic);
+        }
+    }
+}
+
+
+void
+PointToPointBCubeHelper::AssignIpv6Addresses (Ipv6Address addrBase, Ipv6Prefix prefix)
+{
+  Ipv6AddressGenerator::Init (addrBase, prefix);
+  Ipv6Address v6network;
+  Ipv6AddressHelper addrHelper;
+
+  for (uint32_t i = 0; i < m_levelSwitchDevices.size (); ++i)
+    {
+      v6network = Ipv6AddressGenerator::NextNetwork (prefix);
+      addrHelper.SetBase (v6network, prefix);
+      for (uint32_t j = 0; j < m_levelSwitchDevices[i].GetN (); j += 2)
+        {
+          Ipv6InterfaceContainer ic = addrHelper.Assign (m_levelSwitchDevices[i].Get (j));
+          m_serverInterfaces6.Add (ic);
+          ic = addrHelper.Assign (m_levelSwitchDevices[i].Get (j + 1));
+          m_switchInterfaces6[i].Add (ic);
+        }
+    }
+}
+
+Ipv4Address
+PointToPointBCubeHelper::GetServerIpv4Address (uint32_t i) const
+{
+  return m_serverInterfaces.GetAddress (i);
+}
+
+Ipv4Address
+PointToPointBCubeHelper::GetSwitchIpv4Address (uint32_t i, uint32_t j) const
+{
+  return m_switchInterfaces[i].GetAddress (j);
+}
+
+Ipv6Address
+PointToPointBCubeHelper::GetServerIpv6Address (uint32_t i) const
+{
+  return m_serverInterfaces6.GetAddress (i, 1);
+}
+
+Ipv6Address
+PointToPointBCubeHelper::GetSwitchIpv6Address (uint32_t i, uint32_t j) const
+{
+  return m_switchInterfaces6[i].GetAddress (j, 1);
+}
+
+Ptr<Node>
+PointToPointBCubeHelper::GetServerNode (uint32_t i) const
+{
+  return m_servers.Get (i);
+}
+
+Ptr<Node>
+PointToPointBCubeHelper::GetSwitchNode (uint32_t i, uint32_t j) const
+{
+  uint32_t numLevelSwitches = pow (m_numServers,m_numLevels);
+  return m_switches.Get (i * numLevelSwitches + j);
+}
+
+*/
+
+} // namespace ns3
---
+++ src/point-to-point-layout/model/point-to-point-fattree.h
@@ -0,0 +1,195 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2017 NITK Surathkal
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Shravya K.S. <shravya.ks0@gmail.com>
+ *
+ */
+
+#ifndef POINT_TO_POINT_FATTREE_HELPER_H
+#define POINT_TO_POINT_FATTREE_HELPER_H
+
+#include <vector>
+
+#include "internet-stack-helper.h"
+#include "point-to-point-helper.h"
+#include "ipv4-address-helper.h"
+#include "ipv6-address-helper.h"
+#include "ipv4-interface-container.h"
+#include "ipv6-interface-container.h"
+#include "net-device-container.h"
+
+namespace ns3 {
+
+/**
+ * \ingroup point-to-point-layout
+ *
+ * \brief A helper to make it easier to create a Fat-Tree topology
+ * with p2p links
+ */
+class PointToPointFatTreeHelper
+{
+public:
+  /**
+   * Create a PointToPointFatTreeHelper in order to easily create
+   * FatTree topologies using p2p links
+   *
+   * \param numPods total number of Pods in FatTree
+   *
+   * \param pointToPoint the PointToPointHelper which is used
+   *                     to connect all of the nodes together
+   *                     in the FatTree
+   */
+  PointToPointFatTreeHelper (uint32_t numPods,
+                             PointToPointHelper pointToPoint);
+
+  ~PointToPointFatTreeHelper ();
+
+  /**
+   * \param row the row address of the switch desired
+   *
+   * \param col the column address of the switch desired
+   *
+   * \returns a pointer to the switch specified by the
+   *          (row, col) address
+   */
+  Ptr<Node> GetSwitchNode (uint32_t row, uint32_t col) const;
+
+  /**
+   * \param col the column address of the server desired
+   *
+   * \returns a pointer to the server specified by the
+   *          column address
+   */
+  Ptr<Node> GetServerNode (uint32_t col) const;
+
+  /**
+   * This returns an Ipv4 address at the switch specified by
+   * the (row, col) address.  Technically, a switch will have
+   * multiple interfaces in the BCube; therefore, it also has
+   * multiple Ipv4 addresses.  This method only returns one of
+   * the addresses. If you picture the BCube, the address returned
+   * is the lowest index server connected to this switch.
+   *
+   * \param row the row address of the switch desired
+   *
+   * \param col the column address of the switch desired
+   *
+   * \returns Ipv4Address of one of the interfaces of the switch
+   *          specified by the (row, col) address
+   */
+  Ipv4Address GetSwitchIpv4Address (uint32_t row, uint32_t col) const;
+
+  /**
+   * This returns an Ipv6 address at the switch specified by
+   * the (row, col) address.  Technically, a switch will have
+   * multiple interfaces in the BCube; therefore, it also has
+   * multiple Ipv6 addresses.  This method only returns one of
+   * the addresses. If you picture the BCube, the address returned
+   * is the lowest index server connected to this switch.
+   *
+   * \param row the row address of the switch desired
+   *
+   * \param col the column address of the switch desired
+   *
+   * \returns Ipv6Address of one of the interfaces of the switch
+   *          specified by the (row, col) address
+   */
+  Ipv6Address GetSwitchIpv6Address (uint32_t row, uint32_t col) const;
+
+  /**
+   * This returns an Ipv4 address at the server specified by
+   * the col address.  Technically, a server will have
+   * multiple interfaces in the BCube; therefore, it also has
+   * multiple Ipv4 addresses.  This method only returns one of
+   * the addresses. If you picture the BCube, the address returned
+   * is the switch at lowest level connected to this server.
+   *
+   * \param col the column address of the server desired
+   *
+   * \returns Ipv4Address of one of the interfaces of the server
+   *          specified by the col address
+   */
+  Ipv4Address GetServerIpv4Address (uint32_t col) const;
+
+  /**
+   * This returns an Ipv6 address at the server specified by
+   * the col address.  Technically, a server will have
+   * multiple interfaces in the BCube; therefore, it also has
+   * multiple Ipv6 addresses.  This method only returns one of
+   * the addresses. If you picture the BCube, the address returned
+   * is the switch at lowest level connected to this server.
+   *
+   * \param col the column address of the server desired
+   *
+   * \returns Ipv6Address of one of the interfaces of the server
+   *          specified by the col address
+   */
+  Ipv6Address GetServerIpv6Address (uint32_t col) const;
+
+  /**
+   * \param stack an InternetStackHelper which is used to install
+   *              on every node in the BCube
+   */
+  void InstallStack (InternetStackHelper stack);
+
+  /**
+   * Assigns Ipv4 addresses to all the interfaces of switch
+   *
+   * \param network an IPv4 address representing the network portion
+   *                of the IPv4 Address
+   *
+   * \param mask the mask length
+   */
+  void AssignIpv4Addresses (Ipv4Address network, Ipv4Mask mask);
+
+  /**
+   * Assigns Ipv6 addresses to all the interfaces of the switch
+   *
+   * \param network an IPv6 address representing the network portion
+   *                of the IPv6 Address
+   *
+   * \param prefix the prefix length
+   */
+  void AssignIpv6Addresses (Ipv6Address network, Ipv6Prefix prefix);
+
+  /**
+   * Sets up the node canvas locations for every node in the BCube.
+   * This is needed for use with the animation interface
+   *
+   * \param ulx upper left x value
+   * \param uly upper left y value
+   * \param lrx lower right x value
+   * \param lry lower right y value
+   */
+  void BoundingBox (double ulx, double uly, double lrx, double lry);
+
+private:
+  uint32_t m_numPods;                                           //!< number of pods
+  //std::vector<NetDeviceContainer> m_levelSwitchDevices;         //!< Net Device container for servers and switches
+  //std::vector<Ipv4InterfaceContainer> m_switchInterfaces;       //!< IPv4 interfaces of switch
+  //Ipv4InterfaceContainer m_serverInterfaces;                    //!< IPv4 interfaces of server
+  //std::vector<Ipv6InterfaceContainer> m_switchInterfaces6;      //!< IPv6 interfaces of switch
+  //Ipv6InterfaceContainer m_serverInterfaces6;                   //!< IPv6 interfaces of server
+  NodeContainer m_coreSwitches;                                   //!< all the core switches in the fat-tree
+  NodeContainer m_aggregateSwitches;                              //!< all the aggregate switches in the fat-tree
+  NodeContainer m_edgeSwitches;                                   //!< all the edge switches in the fat-tree
+  NodeContainer m_servers;                                        //!< all the servers in the fat-tree
+};
+
+} // namespace ns3
+
+#endif /* POINT_TO_POINT_BCUBE_HELPER_H */
